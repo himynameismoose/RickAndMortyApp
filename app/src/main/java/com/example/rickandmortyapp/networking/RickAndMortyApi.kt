12827080490976c719a27fc8dd0787fa3dfd7f75
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-15 16:17:33 -0700

    create apolloclient

---
+++ app/src/main/java/com/example/rickandmortyapp/networking/RickAndMortyApi.kt
@@ -0,0 +1,19 @@
+package com.example.rickandmortyapp.networking
+
+import android.os.Looper
+import com.apollographql.apollo.ApolloClient
+import okhttp3.OkHttpClient
+
+class RickAndMortyApi {
+    fun getApolloClient(): ApolloClient {
+        check(Looper.myLooper() == Looper.getMainLooper()) {
+            "Only the main thread can get the apolloClient instance"
+        }
+
+        val okHttpClient = OkHttpClient.Builder().build()
+        return ApolloClient.builder()
+            .serverUrl("https://rickandmortyapi.com/graphql")
+            .okHttpClient(okHttpClient)
+            .build()
+    }
+}
