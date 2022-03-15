c1162a3e4cac6c9b058facdf861805e4e9ad5c08
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-15 16:19:16 -0700

    inject hilt

---
+++ app/src/main/java/com/example/rickandmortyapp/di/RepoModule.kt
@@ -0,0 +1,16 @@
+package com.example.rickandmortyapp.di
+
+import com.example.rickandmortyapp.networking.RickAndMortyApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepoModule {
+    @Singleton
+    @Provides
+    fun provideWebService() = RickAndMortyApi()
+}
