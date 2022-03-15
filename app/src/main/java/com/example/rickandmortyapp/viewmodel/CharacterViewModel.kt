329186c7ff435bdb99a52f42c1bae1ede3273a88
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-15 16:57:52 -0700

    update character view model

--- app/src/main/java/com/example/rickandmortyapp/viewmodel/CharacterViewModel.kt
+++ app/src/main/java/com/example/rickandmortyapp/viewmodel/CharacterViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
+import com.example.rickandmortyapp.CharacterQuery
 import com.example.rickandmortyapp.CharactersListQuery
 import com.example.rickandmortyapp.repository.CharacterRepository
 import com.example.rickandmortyapp.view.state.ViewState
@@ -21,9 +22,15 @@ class CharacterViewModel @Inject constructor(
     private val repository: CharacterRepository,
 ) : ViewModel() {
 
+    // for character list
     private val _charactersList by lazy { MutableLiveData<ViewState<Response<CharactersListQuery.Data>>>() }
     val charactersList: LiveData<ViewState<Response<CharactersListQuery.Data>>>
         get() = _charactersList
+
+    // for character
+    private val _character by lazy { MutableLiveData<ViewState<Response<CharacterQuery.Data>>>() }
+    val character: LiveData<ViewState<Response<CharacterQuery.Data>>>
+        get() = _character
 
     fun queryCharactersList() = viewModelScope.launch {
         _charactersList.postValue(ViewState.Loading())
@@ -33,6 +40,17 @@ class CharacterViewModel @Inject constructor(
         } catch (e: ApolloException) {
             Log.d("ApolloException", "Failure", e)
             _charactersList.postValue(ViewState.Error("Error fetching characters"))
+        }
+    }
+
+    fun queryCharacter(id: String) = viewModelScope.launch {
+        _character.postValue(ViewState.Loading())
+        try {
+            val response = repository.queryCharacter(id)
+            _character.postValue(ViewState.Success(response))
+        } catch (ae: ApolloException) {
+            Log.d("ApolloException", "Failure", ae)
+            _character.postValue(ViewState.Error("Error fetching characters"))
         }
     }
 
