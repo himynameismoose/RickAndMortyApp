6737cb11f0d9e649ceb6f83873406833a4b0f496
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-15 16:24:54 -0700

    handle view states

---
+++ app/src/main/java/com/example/rickandmortyapp/view/state/ViewState.kt
@@ -0,0 +1,11 @@
+package com.example.rickandmortyapp.view.state
+
+
+sealed class ViewState<T>(
+    val value: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : ViewState<T>(data)
+    class Error<T>(message: String?, data: T? = null) : ViewState<T>(data, message)
+    class Loading<T> : ViewState<T>()
+}
