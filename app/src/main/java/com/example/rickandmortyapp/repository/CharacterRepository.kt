77e39b41690b7c421e7aa5743e7f1287cf074565
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-15 16:53:29 -0700

    add character query

--- app/src/main/java/com/example/rickandmortyapp/repository/CharacterRepository.kt
+++ app/src/main/java/com/example/rickandmortyapp/repository/CharacterRepository.kt
@@ -1,9 +1,14 @@
 package com.example.rickandmortyapp.repository
 
 import com.apollographql.apollo.api.Response
+import com.example.rickandmortyapp.CharacterQuery
 import com.example.rickandmortyapp.CharactersListQuery
 
 interface CharacterRepository {
 
+    // generates character list
     suspend fun queryCharactersList(): Response<CharactersListQuery.Data>
+
+    // generates character
+    suspend fun queryCharacter(id: String): Response<CharacterQuery.Data>
 }
