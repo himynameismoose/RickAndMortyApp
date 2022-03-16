09329bb6b52122413f56330c642567bb0ae542a1
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2022-03-15 17:13:23 -0700

    update views

---
+++ app/src/main/java/com/example/rickandmortyapp/view/ui/CharacterDetailsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.rickandmortyapp.view.ui
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+@ExperimentalCoroutinesApi
+@AndroidEntryPoint
+class CharacterDetailsFragment : Fragment() {
+}
